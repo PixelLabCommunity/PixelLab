07c1b4b51d77f8fd71d35195fb3a0aae6343c6bb
Slypi1 <94966017+Slypi1@users.noreply.github.com>
2022-08-22 23:33:21 +0700

    Script.InputConroler

---
+++ Unity Games/TZ_Race/Assets/Script/DataHolder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DataHolder
+{
+    private static GameObject prefabName;
+    public static GameObject Prefab
+    {
+        get { return prefabName; }
+        set { prefabName = value; }
+    }
+}
---
+++ Unity Games/TZ_Race/Assets/Script/Road.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//public class Road : MonoBehaviour
+//{
+//    public static Road Instation;
+//    [SerializeField] private Rigidbody _rigidbodyCar;
+//    private Vector3 transformCar;
+//    private Vector3 startformCar;
+//    public bool proverka;
+//    public static event OnSwipeInput SwipeEvent;
+//    public delegate void OnSwipeInput(bool speed);
+
+//    private void Start()
+//    {      
+//        Instation = GetComponent<Road>();    
+//    }
+//    void OnTriggerStay(Collider collision)
+//    {
+//        transformCar = _rigidbodyCar.transform.position;
+//    }
+//    void  OnTriggerExit(Collider collision)
+//    {
+//        SwipeEvent(false);
+//        StartCoroutine(WaitForReturng());            
+//    }
+//    IEnumerator WaitForReturng()
+//    {       
+//        yield return new WaitForSeconds(0.50f);
+//        ReturCar();
+//    }
+//    void ReturCar()
+//    {
+//        _rigidbodyCar.transform.position = transformCar;
+//        _rigidbodyCar.velocity = Vector3.zero;
+//        proverka = false;
+//    }
+
+
+//}
