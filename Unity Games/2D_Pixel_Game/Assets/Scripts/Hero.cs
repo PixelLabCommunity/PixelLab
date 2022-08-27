17b5c94cedeb6de34f34e0c2af6a5f3a6c41faf2
cooler-SAI <cooler19860212@yahoo.com>
2022-08-27 20:52:11 +0300

    Unity/Game: added collider for more real physics check Hero, update GameObjects and added new

--- Unity Games/2D_Pixel_Game/Assets/Scripts/Hero.cs
+++ Unity Games/2D_Pixel_Game/Assets/Scripts/Hero.cs
@@ -4,10 +4,8 @@ public class Hero : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpSpeed;
-    [SerializeField] private LayerMask _groundLayer;
 
-    [SerializeField] private float _groundCheckRadius;
-    [SerializeField] private Vector3 _groundCheckPositionDelta;
+    [SerializeField] private LayerCheck _groundCheck;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _direction;
@@ -26,21 +24,21 @@ public class Hero : MonoBehaviour
     {
         _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
         var isJumping = _direction.y > 0;
-        if (isJumping && IsGrounded())
+        if (isJumping)
         {
-            _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            if (IsGrounded())
+            {
+                _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
+            }
+        }
+        else if (_rigidbody.velocity.y > 0)
+        {
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
         }
     }
 
     private bool IsGrounded()
     {
-        var hit = Physics2D.CircleCast(transform.position + _groundCheckPositionDelta, _groundCheckRadius, Vector2.down, 0, _groundLayer);
-        return hit.collider != null;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = IsGrounded() ? Color.gray : Color.red;
-        Gizmos.DrawSphere(transform.position + _groundCheckPositionDelta, _groundCheckRadius);
+        return _groundCheck.IsTouchingLayer;
     }
 }
