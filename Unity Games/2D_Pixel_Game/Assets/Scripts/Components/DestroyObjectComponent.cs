00e029a97c93596ef666e2d4645e4f802dff05cf
cooler-SAI <cooler19860212@yahoo.com>
2022-08-30 21:45:29 +0300

    Unity/Game: added more support for level reset

---
+++ Unity Games/2D_Pixel_Game/Assets/Scripts/Components/DestroyObjectComponent.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Components
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToDestroy;
+
+        public void DestroyObject()
+        {
+            Destroy(_objectToDestroy);
+        }
+    }
+}
