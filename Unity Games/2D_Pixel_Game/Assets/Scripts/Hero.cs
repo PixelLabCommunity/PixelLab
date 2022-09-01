2ba82d7ee803d80ba6608a3da97846b340e6df8c
cooler-SAI <cooler19860212@yahoo.com>
2022-09-01 17:44:13 +0300

    Unity/Game: added more animations

--- Unity Games/2D_Pixel_Game/Assets/Scripts/Hero.cs
+++ Unity Games/2D_Pixel_Game/Assets/Scripts/Hero.cs
@@ -11,10 +11,13 @@ namespace Scripts
 
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
+        private Animator _animator;
+        private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
 
         private void Awake()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
         public void SetDirection(Vector2 direction)
@@ -26,9 +29,11 @@ namespace Scripts
         {
             _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
             var isJumping = _direction.y > 0;
+            var isGrounded = IsGrounded();
+
             if (isJumping)
             {
-                if (IsGrounded() && _rigidbody.velocity.y <= 0)
+                if (isGrounded && _rigidbody.velocity.y <= 0)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
                 }
@@ -37,6 +42,9 @@ namespace Scripts
             {
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
+            _animator.SetBool(IsGroundKey, isGrounded);
+            _animator.SetBool("is-running", _direction.x != 0);
+            _animator.SetFloat("vertical-velocity", _rigidbody.velocity.y);
         }
 
         private bool IsGrounded()
