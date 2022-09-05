6982e3243f79f03ca7f242ddea6cf97e3150520f
cooler-SAI <cooler19860212@yahoo.com>
2022-09-05 15:00:33 +0300

    Unity/Game: added double jump for Hero

--- Unity Games/2D_Pixel_Game/Assets/Scripts/Hero.cs
+++ Unity Games/2D_Pixel_Game/Assets/Scripts/Hero.cs
@@ -8,14 +8,16 @@ namespace Scripts
         [SerializeField] private float _jumpSpeed;
         [SerializeField] private LayerCheck _groundCheck;
 
+        private SpriteRenderer _spriteRenderer;
+        private bool _isGrounded;
         private Rigidbody2D _rigidbody;
         private Vector2 _direction;
         private Animator _animator;
+        private bool _allowDoubleJump;
+
         private static readonly int IsGroundKey = Animator.StringToHash("is-ground");
         private static readonly int IsRunningKey = Animator.StringToHash("is-running");
         private static readonly int VerticalVelocityKey = Animator.StringToHash("vertical-velocity");
-
-        private SpriteRenderer _spriteRenderer;
 
         private void Awake()
         {
@@ -31,27 +33,59 @@ namespace Scripts
 
         private void FixedUpdate()
         {
-            _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
-            var isJumping = _direction.y > 0;
-            var isGrounded = IsGrounded();
+            var xVelocity = _direction.x * _speed;
+            var yVelocity = CalculateYVelovity();
+            _rigidbody.velocity = new Vector2(xVelocity, yVelocity);
 
-            if (isJumping)
+            // Animation
+            _animator.SetBool(IsGroundKey, _isGrounded);
+            _animator.SetBool(IsRunningKey, _direction.x != 0);
+            _animator.SetFloat(VerticalVelocityKey, _rigidbody.velocity.y);
+            UpdateSpriteDirection();
+        }
+
+        private float CalculateYVelovity()
+        {
+            var isJumpPressing = _direction.y > 0;
+            var yVelocity = _rigidbody.velocity.y;
+
+            if (_isGrounded) _allowDoubleJump = true;
+            if (isJumpPressing)
             {
-                if (isGrounded && _rigidbody.velocity.y <= 0)
+                yVelocity = CalculateJumpVelovity(yVelocity);
+                if (_isGrounded && _rigidbody.velocity.y <= 0.001f)
                 {
                     _rigidbody.AddForce(Vector2.up * _jumpSpeed, ForceMode2D.Impulse);
                 }
             }
             else if (_rigidbody.velocity.y > 0)
             {
+                yVelocity *= 0.5f;
                 _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
             }
+            return yVelocity;
+        }
 
-            // Animation
-            _animator.SetBool(IsGroundKey, isGrounded);
-            _animator.SetBool(IsRunningKey, _direction.x != 0);
-            _animator.SetFloat(VerticalVelocityKey, _rigidbody.velocity.y);
-            UpdateSpriteDirection();
+        private float CalculateJumpVelovity(float yVelocity)
+        {
+            var isFalling = _rigidbody.velocity.y <= 0.001f;
+            if (!isFalling) return yVelocity;
+
+            if (_isGrounded)
+            {
+                yVelocity += _jumpSpeed;
+            }
+            else if (_allowDoubleJump)
+            {
+                yVelocity = _jumpSpeed;
+                _allowDoubleJump = false;
+            }
+            return yVelocity;
+        }
+
+        private void Update()
+        {
+            _isGrounded = IsGrounded();
         }
 
         /// <summary>
