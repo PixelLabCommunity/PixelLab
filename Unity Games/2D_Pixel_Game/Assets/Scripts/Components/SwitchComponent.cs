bdde7063213d04baa12cc73ab6b2725ef78b81ba
cooler-SAI <cooler19860212@yahoo.com>
2022-09-09 23:00:21 +0300

    Unity/GameObject: added switch animation and door collider, all works pretty good

---
+++ Unity Games/2D_Pixel_Game/Assets/Scripts/Components/SwitchComponent.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scripts.Components
+{
+    public class SwitchComponent : MonoBehaviour
+    {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private bool _state;
+        [SerializeField] private string _animationKey;
+
+        public void Switch()
+        {
+            _state = !_state;
+            _animator.SetBool(_animationKey, _state);
+        }
+
+        [ContextMenu("Switch")]
+        public void SwitchIt()
+        {
+            Switch();
+        }
+    }
+}
