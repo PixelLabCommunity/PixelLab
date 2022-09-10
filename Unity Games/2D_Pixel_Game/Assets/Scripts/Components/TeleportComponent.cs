78d9a00b054963774e9740ef566eb9d35a379239
cooler-SAI <cooler19860212@yahoo.com>
2022-09-10 21:55:28 +0300

    Unity/GameObject: added teleport

---
+++ Unity Games/2D_Pixel_Game/Assets/Scripts/Components/TeleportComponent.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Scripts.Components
+{
+    public class TeleportComponent : MonoBehaviour
+    {
+        [SerializeField] private Transform _targetPosition;
+
+        public void Teleport(GameObject target)
+        {
+            target.transform.position = _targetPosition.position;
+        }
+    }
+}
