7b8561aa8bba17dcfc2153fd4db7f86cc1148a0f
cooler-SAI <cooler19860212@yahoo.com>
2022-09-29 16:58:04 +0300

    Game/Pixel-Pirate-2D: improved moving Platform

--- Unity Games/Pixel-Pirate-2D/Assets/Scripts/Platform.cs
+++ Unity Games/Pixel-Pirate-2D/Assets/Scripts/Platform.cs
@@ -3,34 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class Platform : MonoBehaviour
+namespace Scripts
 {
-    [SerializeField] private Transform _pos1, _pos2;
-    [SerializeField] private float _speed;
-    [SerializeField] private Transform _startPos;
-
-    private Vector3 _nextPos;
-
-    private void Start()
+    public class Platform : MonoBehaviour
     {
-        _nextPos = _startPos.position;
-    }
+        [SerializeField] private Transform _pos1, _pos2;
+        [SerializeField] private float _speed;
+        [SerializeField] private Transform _startPos;
 
-    private void Update()
-    {
-        if (transform.position == _pos1.position)
+        private Vector3 _nextPos;
+
+        private void Start()
         {
-            _nextPos = _pos2.position;
+            _nextPos = _startPos.position;
         }
-        if (transform.position == _pos2.position)
-        {
-            _nextPos = _pos1.position;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, _nextPos, _speed * Time.deltaTime);
-    }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(_pos1.position, _pos2.position);
+        private void Update()
+        {
+            if (transform.position == _pos1.position)
+            {
+                _nextPos = _pos2.position;
+            }
+            if (transform.position == _pos2.position)
+            {
+                _nextPos = _pos1.position;
+            }
+            transform.position = Vector3.MoveTowards(transform.position, _nextPos, _speed * Time.deltaTime);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(_pos1.position, _pos2.position);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.name == "HeroPirate")
+            {
+                collision.gameObject.transform.SetParent(transform);
+            }
+        }
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.name == "HeroPirate")
+            {
+                collision.gameObject.transform.SetParent(null);
+            }
+        }
     }
 }
