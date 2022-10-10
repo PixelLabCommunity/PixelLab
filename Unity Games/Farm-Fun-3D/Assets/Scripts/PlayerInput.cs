d292cf939ece4fcbb3600bb2020617cb3eeb82e0
cooler-SAI <cooler19860212@yahoo.com>
2022-10-10 20:17:10 +0300

    Game/Farm-Fun-3D: added more Environment, set position of the Player, added Layers

--- Unity Games/Farm-Fun-3D/Assets/Scripts/PlayerInput.cs
+++ Unity Games/Farm-Fun-3D/Assets/Scripts/PlayerInput.cs
@@ -6,15 +6,22 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private float _speed;
     private float _horizontalInput;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-    }
+    private readonly float _xRange = 16.5f;
 
     private void Update()
     {
+        // Keep Player in bounds
+        if (transform.localPosition.x <= -_xRange)
+        {
+            transform.localPosition = new Vector3(-_xRange, transform.localPosition.y, transform.localPosition.z);
+        }
+        if (transform.localPosition.x >= _xRange)
+        {
+            transform.localPosition = new Vector3(_xRange, transform.localPosition.y, transform.localPosition.z);
+        }
+
+        // Keep Player move
         _horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * _horizontalInput * _speed);
+        transform.Translate(_horizontalInput * _speed * Time.deltaTime * Vector3.right);
     }
 }
