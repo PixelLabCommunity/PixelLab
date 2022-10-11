5438cd68b20096e5488b08b5829293d282006e2e
cooler-SAI <cooler19860212@yahoo.com>
2022-10-12 00:35:01 +0300

    Game/Farm-Fun-3D: added animals to the scene, create prefabs, added more scripts

--- Unity Games/Farm-Fun-3D/Assets/Scripts/PlayerInput.cs
+++ Unity Games/Farm-Fun-3D/Assets/Scripts/PlayerInput.cs
@@ -7,11 +7,25 @@ public class PlayerInput : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private GameObject _prefabPizza;
 
-
     private float _horizontalInput;
     private readonly float _xRange = 16.5f;
 
     private void Update()
+    {
+        PlayerBounds();
+
+        // Keep Player move
+        _horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(_horizontalInput * _speed * Time.deltaTime * Vector3.right);
+
+        // Keep Player throw pizza
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_prefabPizza, transform.position, _prefabPizza.transform.rotation);
+        }
+    }
+
+    private void PlayerBounds()
     {
         // Keep Player in bounds
         if (transform.localPosition.x <= -_xRange)
@@ -22,9 +36,5 @@ public class PlayerInput : MonoBehaviour
         {
             transform.localPosition = new Vector3(_xRange, transform.localPosition.y, transform.localPosition.z);
         }
-
-        // Keep Player move
-        _horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(_horizontalInput * _speed * Time.deltaTime * Vector3.right);
     }
 }
