12968e215ff7aa0b6df63eceb5b5cd4dc9405eb3
cooler-SAI <cooler19860212@yahoo.com>
2022-10-13 14:44:52 +0300

    Game/Farm-Fun-3D: added random automatic spawn animals

--- Unity Games/Farm-Fun-3D/Assets/Scripts/SpawnManager.cs
+++ Unity Games/Farm-Fun-3D/Assets/Scripts/SpawnManager.cs
@@ -8,19 +8,26 @@ public class SpawnManager : MonoBehaviour
 
     private float _spawnRangeX = 15.0f;
     private float _spawnPosZ = 20.0f;
+    private float _startDelay = 2.0f;
+    private float _repeatInterval = 1.5f;
 
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal",_startDelay, _repeatInterval);
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // Random spawn animals in axes X
-            Vector3 _spawnPos = new(Random.Range(-_spawnRangeX, _spawnRangeX),
-                transform.position.y, _spawnPosZ);
+    }
 
-            // Random spawn animals types
-            int _animalsIndex = Random.Range(0, _animalPrefabs.Length);
-            Instantiate(_animalPrefabs[_animalsIndex], _spawnPos,
-                _animalPrefabs[_animalsIndex].transform.rotation);
-        }
+    private void SpawnRandomAnimal()
+    {
+        // Random spawn animals in axes X
+        Vector3 _spawnPos = new(Random.Range(-_spawnRangeX, _spawnRangeX),
+            transform.position.y, _spawnPosZ);
+
+        // Random spawn animals types
+        int _animalsIndex = Random.Range(0, _animalPrefabs.Length);
+        Instantiate(_animalPrefabs[_animalsIndex], _spawnPos,
+            _animalPrefabs[_animalsIndex].transform.rotation);
     }
 }
