491479b47d74b7b62bf7a4c53a73ca05df99e8de
cooler-SAI <cooler19860212@yahoo.com>
2022-10-13 12:03:20 +0300

    Game/Farm-Fun-3D: added random range spawn animals, controls, code optimized

--- Unity Games/Farm-Fun-3D/Assets/Scripts/SpawnManager.cs
+++ Unity Games/Farm-Fun-3D/Assets/Scripts/SpawnManager.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] _animalPrefabs;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int _animalsIndex = Random.Range(0, _animalPrefabs.Length);
+            Instantiate(_animalPrefabs[_animalsIndex], new Vector3(transform.position.x, transform.position.y, 20),
+                _animalPrefabs[_animalsIndex].transform.rotation);
+        }
+    }
 }
