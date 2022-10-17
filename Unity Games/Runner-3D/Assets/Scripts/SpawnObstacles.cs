3548274025bc6bdd3d2e0bad80284b3ef6de7711
cooler-SAI <cooler19860212@yahoo.com>
2022-10-17 15:35:42 +0300

    Game/Runner-3D: added gameObjects spawns in time

--- Unity Games/Runner-3D/Assets/Scripts/SpawnObstacles.cs
+++ Unity Games/Runner-3D/Assets/Scripts/SpawnObstacles.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class SpawnObstacles : MonoBehaviour
 {
     [SerializeField] private GameObject[] _obstaclePrefabs;
+
     private Vector3 _spawnObstacles = new Vector3(19.85f, 0.0f, 0.0f);
+    private float _startDelay = 2.0f;
+    private float _repeatRange = 2.0f;
 
     private void Start()
+    {
+        InvokeRepeating("SpawnPrefabs", _startDelay, _repeatRange);
+    }
+
+    private void SpawnPrefabs()
     {
         int _spawnPrefabs = Random.Range(0, _obstaclePrefabs.Length);
         Instantiate(_obstaclePrefabs[_spawnPrefabs], _spawnObstacles,
             _obstaclePrefabs[_spawnPrefabs].transform.rotation);
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
     }
 }
