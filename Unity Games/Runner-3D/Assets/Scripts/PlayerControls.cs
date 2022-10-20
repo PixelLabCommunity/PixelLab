f0ed6e658dbc4b2f5da060a67ddce9679abefb06
cooler-SAI <cooler19860212@yahoo.com>
2022-10-20 18:44:12 +0300

    Game/Runner-3D: added gravity settings, set controls

--- Unity Games/Runner-3D/Assets/Scripts/PlayerControls.cs
+++ Unity Games/Runner-3D/Assets/Scripts/PlayerControls.cs
@@ -1,29 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] private float _jumpForce = 10.0f;
-    [SerializeField] private float _gravityModifyer = 1.0f;
-    public bool _gameOver;
+    [SerializeField] private float _gravityValue = 18.0f;
 
+    public bool _gameOver;
     private Rigidbody _playerRb;
     private bool _isOnGround = true;
 
     private void Start()
     {
         _playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= _gravityModifyer;
+        Physics.gravity = new Vector3(0.0f, -_gravityValue, 0.0f);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        if (_gameOver == false)
         {
-            _playerRb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
-            _isOnGround = false;
+            PlayerJump();
         }
+    }
+
+    private void LateUpdate()
+    {
+        RestartLevel();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -35,7 +40,25 @@ public class PlayerControls : MonoBehaviour
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             _gameOver = true;
-            Debug.Log("Game Over!");
+            Debug.Log("Game Over! Press 'ESC' for restart the Game!");
+        }
+    }
+
+    private void PlayerJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _isOnGround)
+        {
+            _playerRb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _isOnGround = false;
+        }
+    }
+
+    private void RestartLevel()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            var _scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(_scene.name);
         }
     }
 }
