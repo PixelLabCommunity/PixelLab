667172f9c028b99be3c428603c55410da92ffc41
cooler-SAI <cooler19860212@yahoo.com>
2022-10-26 21:55:14 +0300

    Game/Car-on-Road-3D: quick update

--- Unity Games/Lab-3D/Assets/Scripts/PlayerController.cs
+++ Unity Games/Lab-3D/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody _playerRigidbody;
     private Vector3 _moveDirections = Vector3.zero;
+    private Animator _animator;
 
     private float _zBound = 12.0f;
 
@@ -31,21 +32,27 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _moveDirections = _playerControls.ReadValue<Vector3>();
-        if (_playerRigidbody.position.z > _zBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y,
-                _zBound);
-        }
-        if (_playerRigidbody.position.z < -_zBound)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y,
-                -_zBound);
-        }
+
+        PlayerBounds();
     }
 
     private void FixedUpdate()
     {
         _playerRigidbody.velocity = new Vector3(_moveDirections.x * _speed,
            _moveDirections.y, _moveDirections.z * _speed);
+    }
+
+    private void PlayerBounds()
+    {
+        if (_playerRigidbody.position.z > _zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+                _zBound);
+        }
+        else if (_playerRigidbody.position.z < -_zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y,
+                -_zBound);
+        }
     }
 }
