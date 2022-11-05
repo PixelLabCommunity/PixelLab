88325797d61cab7428f9c2ca41c490b29ffd2b41
cooler-SAI <cooler19860212@yahoo.com>
2022-11-05 23:19:43 +0300

    Game/Lab-1-3D: set enemy destroy state and movement state, also add speed

---
+++ Unity Games/Lab-3D/Assets/Scripts/MoveDown.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDown : MonoBehaviour
+{
+    [SerializeField] private float _enemySpeed;
+
+    private Rigidbody _enemyRB;
+    private float _positionBoundZ = -15.0f;
+
+    private void Start()
+    {
+        _enemyRB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _enemyRB.AddForce(Vector3.forward * -_enemySpeed);
+
+        DestroyEnemy();
+    }
+
+    private void DestroyEnemy()
+    {
+        if (transform.position.z < _positionBoundZ)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
