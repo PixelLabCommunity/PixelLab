91bf0f746a49e8bacfc451f7395ff27fd46efc4a
cooler-SAI <cooler19860212@yahoo.com>
2022-11-10 12:57:27 +0300

    Game/Click-and-Flick-3D: clean up and optimize code

--- Unity Games/Click-and-Flick-3D/Assets/Scripts/Target.cs
+++ Unity Games/Click-and-Flick-3D/Assets/Scripts/Target.cs
@@ -6,13 +6,37 @@ public class Target : MonoBehaviour
 {
     private Rigidbody _targetRb;
 
+    private float _minForce = 12.0f;
+    private float _maxForce = 16.0f;
+    private float _valueTorque = 10.0f;
+    private float _spawnX = 4.0f;
+    private float _spawnY = -6.0f;
+
     private void Start()
     {
         _targetRb = GetComponent<Rigidbody>();
 
-        _targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        _targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+        RandomForce();
+        RandomTorque();
 
-        _targetRb.position = new Vector3(Random.Range(-4, 4), -6);
+        SpawnPosition();
+    }
+
+    private void RandomForce()
+    {
+        float _randForceUp = Random.Range(_minForce, _maxForce);
+        _targetRb.AddForce(Vector3.up * _randForceUp, ForceMode.Impulse);
+    }
+
+    private void RandomTorque()
+    {
+        float _randTorque = Random.Range(-_valueTorque, _valueTorque);
+        _targetRb.AddTorque(_randTorque, _randTorque, _randTorque);
+    }
+
+    private void SpawnPosition()
+    {
+        float _randSpawnX = Random.Range(-_spawnX, _spawnX);
+        _targetRb.position = new Vector3(_randSpawnX, _spawnY);
     }
 }
