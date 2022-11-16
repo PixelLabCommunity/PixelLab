28abf2ec88274c5e16c1857fcfe3eed3c3c7316d
cooler-SAI <cooler19860212@yahoo.com>
2022-11-17 00:17:21 +0300

    Game/Click-and-Flick-3D: added script for the score

--- Unity Games/Click-and-Flick-3D/Assets/Scripts/GameManager.cs
+++ Unity Games/Click-and-Flick-3D/Assets/Scripts/GameManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _spawnTargets;
+    [SerializeField] private TextMeshProUGUI _scoreText;
 
     private float _spawnTime = 1.0f;
     private int _spawnCountMin = 0;
+    private int _score = 0;
+    private int _scoreOnStart = 0;
 
     private void Start()
     {
         StartCoroutine(SpawnTarget());
+        UpdateScore(_scoreOnStart);
     }
 
     private IEnumerator SpawnTarget()
@@ -22,6 +27,13 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(_spawnTime);
             int _randPrefab = Random.Range(_spawnCountMin, _spawnTargets.Count);
             Instantiate(_spawnTargets[_randPrefab]);
+            UpdateScore(5);
         }
+    }
+
+    private void UpdateScore(int _scoreToAdd)
+    {
+        _score += _scoreToAdd;
+        _scoreText.text = "Score: " + _score;
     }
 }
