805532808c5e40babdb80e235293740e009340f1
cooler-SAI <cooler19860212@yahoo.com>
2023-04-29 21:30:18 +0300

    Learning/Alien Video Game Shop - 3D: added URP to the scene, improve Input system

---
+++ Unity Learning/Alien Video Game Shop - 3D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidBody;
+    [SerializeField] private Vector3 _moveInput;
+
+}
