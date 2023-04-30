f2df949676f03b8d197f96147c568dc8872f8d35
cooler-SAI <cooler19860212@yahoo.com>
2023-04-30 22:50:44 +0300

    Learning/AR_PixelLab_Project: deploy scene, added scene objects, materials and other stuff

--- Unity Learning/AR_PixelLab_Project/Assets/Settings/Project Configuration/SceneTemplate_RotateCube.cs
+++ Unity Learning/AR_PixelLab_Project/Assets/Settings/Project Configuration/SceneTemplate_RotateCube.cs
@@ -2,21 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-// Rotates the cube in the template scene
 public class SceneTemplate_RotateCube : MonoBehaviour
 {
-    [Tooltip ("Changes the rotation speed of the cube")]
-    public float rotateSpeed = 1f;
+    [SerializeField] private float _rotateSpeed = 1.0f;
+    [SerializeField] private Vector3 _cubeRotation;
 
-    [Tooltip("Changes orientation of the cube")]
-    public Vector3 objectRotation;
-
-    //Called every frame the app is running
-    // Note that "*" represents multiplication
-    void Update()
+    private void Update()
     {
-    //Change the rotation (by the defined orientation * the time that has passed * defined speed)
-        transform.Rotate(objectRotation * Time.deltaTime * rotateSpeed); 
+        transform.Rotate(_cubeRotation * Time.deltaTime * _rotateSpeed); 
     }
 }
