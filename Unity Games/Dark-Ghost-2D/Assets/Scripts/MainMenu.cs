5ba16d2bfb211495f680b4e201c1262ca60cd74e
cooler-SAI <cooler19860212@yahoo.com>
2023-05-17 22:39:23 +0300

    Game/Dark-Ghost-2D: added new scene, update code, added main menu, added options, added scene change function

---
+++ Unity Games/Dark-Ghost-2D/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private int _buildIndexClose = 1;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + _buildIndexClose);
+        Debug.Log("Game Started");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Closed");
+    }
+}
