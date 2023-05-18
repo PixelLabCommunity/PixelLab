763e79477703dac195d6662ecc693dceb9d25fec
cooler-SAI <cooler19860212@yahoo.com>
2023-05-18 22:59:03 +0300

    Game/Dark-Ghost-2D: added charged bullet, improved code, different fixes

---
+++ Unity Games/Dark-Ghost-2D/Assets/Scripts/HoldButtonEvent.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace Scripts
+{
+    public class HoldButtonEvent : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        public UnityEvent onHoldEvent;
+        public float holdDuration = 1f;
+
+        private bool isPointerDown = false;
+        private float holdTimer = 0f;
+
+        private void Update()
+        {
+            if (isPointerDown)
+            {
+                holdTimer += Time.deltaTime;
+
+                if (holdTimer >= holdDuration)
+                {
+                    onHoldEvent.Invoke();
+                    ResetHold();
+                }
+            }
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            isPointerDown = true;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            ResetHold();
+        }
+
+        private void ResetHold()
+        {
+            isPointerDown = false;
+            holdTimer = 0f;
+        }
+    }
+}
