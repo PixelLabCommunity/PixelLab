084bad5e1595cac711f1979070c5ab8703883a8a
cooler-SAI <cooler19860212@yahoo.com>
2023-08-03 23:43:12 +0300

    Games/John's-LemonHH-3D: added character movements, update build for scripts

--- Unity Games/John's-LemonHH-3D/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
+++ Unity Games/John's-LemonHH-3D/Assets/UnityTechnologies/3DBeginnerTutorial/Scripts/PlayerMovement.cs
@@ -1,56 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed = 20f;
+    private const float MovementY = 0f;
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    [SerializeField] private float turnSpeed = 20f;
 
-    Animator m_Animator;
-    Rigidbody m_Rigidbody;
-    AudioSource m_AudioSource;
-    Vector3 m_Movement;
-    Quaternion m_Rotation = Quaternion.identity;
+    private Animator _mAnimator;
+    private AudioSource _mAudioSource;
+    private Vector3 _mMovement;
+    private Rigidbody _mRigidbody;
+    private Quaternion _mRotation = Quaternion.identity;
 
-    void Start ()
+    private void Start()
     {
-        m_Animator = GetComponent<Animator> ();
-        m_Rigidbody = GetComponent<Rigidbody> ();
-        m_AudioSource = GetComponent<AudioSource> ();
+        _mAnimator = GetComponent<Animator>();
+        _mRigidbody = GetComponent<Rigidbody>();
+        _mAudioSource = GetComponent<AudioSource>();
     }
 
-    void FixedUpdate ()
+    private void FixedUpdate()
     {
-        float horizontal = Input.GetAxis ("Horizontal");
-        float vertical = Input.GetAxis ("Vertical");
-        
-        m_Movement.Set(horizontal, 0f, vertical);
-        m_Movement.Normalize ();
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
 
-        bool hasHorizontalInput = !Mathf.Approximately (horizontal, 0f);
-        bool hasVerticalInput = !Mathf.Approximately (vertical, 0f);
-        bool isWalking = hasHorizontalInput || hasVerticalInput;
-        m_Animator.SetBool ("IsWalking", isWalking);
-        
+        _mMovement.Set(horizontal, MovementY, vertical);
+        _mMovement.Normalize();
+
+        var hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        var hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        var isWalking = hasHorizontalInput || hasVerticalInput;
+        _mAnimator.SetBool(IsWalking, isWalking);
+
         if (isWalking)
         {
-            if (!m_AudioSource.isPlaying)
-            {
-                m_AudioSource.Play();
-            }
+            if (!_mAudioSource.isPlaying) _mAudioSource.Play();
         }
         else
         {
-            m_AudioSource.Stop ();
+            _mAudioSource.Stop();
         }
 
-        Vector3 desiredForward = Vector3.RotateTowards (transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
-        m_Rotation = Quaternion.LookRotation (desiredForward);
+        var desiredForward = Vector3.RotateTowards(transform.forward, _mMovement, turnSpeed * Time.deltaTime, 0f);
+        _mRotation = Quaternion.LookRotation(desiredForward);
     }
 
-    void OnAnimatorMove ()
+    private void OnAnimatorMove()
     {
-        m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
-        m_Rigidbody.MoveRotation (m_Rotation);
+        _mRigidbody.MovePosition(_mRigidbody.position + _mMovement * _mAnimator.deltaPosition.magnitude);
+        _mRigidbody.MoveRotation(_mRotation);
     }
 }
