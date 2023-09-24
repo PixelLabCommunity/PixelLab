f9187d2b5b0f9b640663d6b427cb57ad391c079d
cooler-SAI <cooler19860212@yahoo.com>
2023-09-24 23:38:54 +0300

    Godot/Learning-3D: update player controls script

--- Godot Development/Learning/Learning-3D/scripts/Player.cs
+++ Godot Development/Learning/Learning-3D/scripts/Player.cs
@@ -3,52 +3,45 @@ using Godot;
 namespace Learning3D.scripts;
 
 public partial class Player : CharacterBody3D
-{	
-	private float _currentSpeed = 5.0f;
+{
 	private const float WalkingSpeed = 5.0f;
 	private const float SprintingSpeed = 10.0f;
 	private const float CrouchingSpeed = 3.0f;
 	private const float JumpVelocity = 4.5f;
 	private const float MouseSense = 0.4f;
-	
+	private float _currentSpeed = 5.0f;
+
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	private float _gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
-	[Export]
-	private NodePath _headPath; // This will store the path to the 'head' node.
-
-	private Node3D _head; // This will store the reference to the 'head' node.
+	private Node3D _head;
 
 	public override void _Input(InputEvent @event)
 	{
 		if (@event is not InputEventMouseMotion mouseMotion) return;
 		var rotationRadians = -Mathf.DegToRad(mouseMotion.Relative.X * MouseSense);
 		RotateY(rotationRadians);
+		var rotationRadiansHead = -Mathf.DegToRad(mouseMotion.Relative.Y * MouseSense);
+		_head.RotateX(rotationRadiansHead);
 	}
-	
+
 	public override void _Ready()
 	{
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 
-		// Initialize the 'head' node using the path.
-		_head = GetNode<Node3D>(_headPath);
+		_head = GetNode<Node3D>("head"); // Initialize the "_head" variable when the node is ready
 	}
-	
+
+
 	public override void _PhysicsProcess(double delta)
 	{
 		if (Input.IsActionPressed("sprint"))
-		{
 			_currentSpeed = SprintingSpeed;
-		}
 		// Check if the crouch action is pressed (and not sprinting).
 		else if (Input.IsActionPressed("crouch"))
-		{
 			_currentSpeed = CrouchingSpeed;
-		}
 		else
-		{
 			_currentSpeed = WalkingSpeed;
-		}
 
 		var velocity = Velocity;
 
