474345da8b185fbe34f9a152276c9e5762c04dcd
cooler-SAI <cooler19860212@yahoo.com>
2023-09-30 15:10:30 +0300

    Godot/Sprite-2D: update input script

--- Godot Development/Learning/Sprite-2D/Scripts/Sprite2D.cs
+++ Godot Development/Learning/Sprite-2D/Scripts/Sprite2D.cs
@@ -17,7 +17,8 @@ public partial class Sprite2D : Godot.Sprite2D
 
 	public override void _Process(double delta)
 	{
-		PixelMovement();
+		//PixelMovement();
+		LegacyMovement();
 	}
 
 	private void PixelMovement()
@@ -27,5 +28,18 @@ public partial class Sprite2D : Godot.Sprite2D
 
 		if (Input.IsKeyPressed(Key.A)) Position += new Vector2(-_positionX, _baseY);
 		if (Input.IsKeyPressed(Key.D)) Position += new Vector2(_positionX, _baseY);
+	}
+
+	private void LegacyMovement()
+	{
+		if (Input.IsActionPressed("ui_up") || Input.IsActionPressed("up"))
+			Position += new Vector2(_baseX, -_positionY);
+		if (Input.IsActionPressed("ui_down") || Input.IsActionPressed("down"))
+			Position += new Vector2(_baseX, _positionY);
+
+		if (Input.IsActionPressed("ui_left") || Input.IsActionPressed("left"))
+			Position += new Vector2(-_positionX, _baseY);
+		if (Input.IsActionPressed("ui_right") || Input.IsActionPressed("right"))
+			Position += new Vector2(_positionX, _baseY);
 	}
 }
