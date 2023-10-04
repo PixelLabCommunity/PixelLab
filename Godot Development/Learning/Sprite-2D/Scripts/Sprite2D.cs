5a7cda3d41d8ab9ec01144a9b622e1c5a3b0799a
cooler-SAI <cooler19860212@yahoo.com>
2023-10-04 11:32:24 +0300

    Godot/Sprite-2D: update Sprite script for use child nodes

--- Godot Development/Learning/Sprite-2D/Scripts/Sprite2D.cs
+++ Godot Development/Learning/Sprite-2D/Scripts/Sprite2D.cs
@@ -6,9 +6,13 @@ public partial class Sprite2D : Godot.Sprite2D
 {
 	private Vector2 _basePosition;
 	private float _baseX;
+
 	private float _baseY;
+
+	// private ChildSprite2D _childNode;
 	private float _positionX = 5.0f;
 	private float _positionY = 5.0f;
+
 
 	public override void _Ready()
 	{
@@ -23,13 +27,22 @@ public partial class Sprite2D : Godot.Sprite2D
 
 	private void PixelMovementKeys()
 	{
-		Godot.Sprite2D child = GetNode<ChildSprite2D>("ChildSprite2D");
+		var child = GetNode<Godot.Sprite2D>("ChildSprite2D");
+		var secondChild = GetNode<Godot.Sprite2D>("SecondChildSprite2D");
 
 		if (Input.IsActionPressed("ui_up") || Input.IsActionPressed("up"))
+		{
 			Position += new Vector2(_baseX, -_positionY);
-		child.GlobalPosition = new Vector2(0, 0);
+			child.GlobalPosition = new Vector2(0, 0);
+		}
+
+
 		if (Input.IsActionPressed("ui_down") || Input.IsActionPressed("down"))
+		{
 			Position += new Vector2(_baseX, _positionY);
+			secondChild.GlobalPosition = new Vector2(5, -5);
+		}
+
 
 		if (Input.IsActionPressed("ui_left") || Input.IsActionPressed("left"))
 			Position += new Vector2(-_positionX, _baseY);
