14cb8560cc7d15abbb42d1acd2fe6a69e600c5e2
cooler-SAI <cooler19860212@yahoo.com>
2023-10-11 19:23:50 +0300

    Unity/Lab-3D: test script function

--- Unity Games/Lab-3D/Assets/Scripts/Simple.cs
+++ Unity Games/Lab-3D/Assets/Scripts/Simple.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class Simple : MonoBehaviour
 {
     private int _myInt = 5;
+    private int _myIntSum = 100;
 
     private void Start()
     {
         _myInt = MultiplyNumber(_myInt);
-        Debug.Log(_myInt);
+        _myIntSum = SummNumber(_myIntSum);
+        Debug.Log("Multiply number is: " + _myInt);
+        Debug.Log("Sum number is: " + _myIntSum);
     }
 
     private int MultiplyNumber(int number)
     {
         int result;
         result = number * 2;
+        return result;
+    }
+
+    private int SummNumber(int number)
+    {
+        int result;
+        result = number + 100;
         return result;
     }
 }
