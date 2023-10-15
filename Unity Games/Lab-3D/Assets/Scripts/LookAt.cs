164313d82724e67857f5aa8e8c30cbe2f5118f14
cooler-SAI <cooler19860212@yahoo.com>
2023-10-15 19:18:45 +0300

    Unity/Lab-3D: added experimental new method

---
+++ Unity Games/Lab-3D/Assets/Scripts/LookAt.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void Update()
+    {
+        transform.LookAt(target);
+    }
+}
